b2a3e00bac3bd894f3209fcb32ca13bab66ce0e3
desplega <desplega@desplega.com>
2022-10-22 20:32:13 +0200

    First version

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//var urlBase = 'http://coches/api';
+var urlBase = 'http://larabikes.wsl/api';
+
+function getId() {
+    var url = location.href;
+    var regexp = /\?id=\d{1,}/;
+    var resultados = regexp.exec(url);
+
+    if (resultados.length > 0)
+        return resultados[0].split("=")[1];
+    else
+        return undefined;
+}
