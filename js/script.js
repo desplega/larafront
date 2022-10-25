839f225cef436f7faaae392ede368231cbc0a0f7
desplega <desplega@desplega.com>
2022-10-25 15:33:48 +0200

    Test server

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-//var urlBase = 'http://coches/api';
-var urlBase = 'http://larabikes.wsl/api';
+var urlBase = 'http://coches/api';
+//var urlBase = 'http://larabikes.wsl/api';
 
 function getId() {
     var url = location.href;
